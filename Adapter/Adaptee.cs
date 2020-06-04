ffdfef2bb2e76865ff0b301b2938421fe92318d3
xialei1900 <574892525@qq.com>
2020-06-04 14:28:30 +0800

    add adapter pattern

---
+++ Adapter/Adaptee.cs
@@ -0,0 +1,11 @@
+﻿namespace Adapter
+{
+    //源目标，被适配者
+    public class Adaptee
+    {
+        public string GetSpecificRequest()
+        {
+            return "Specific Request";
+        }
+    }
+}
---
+++ Adapter/Adapter.cs
@@ -0,0 +1,16 @@
+﻿namespace Adapter
+{
+    public class Adapter:ITarget
+    {
+        private readonly Adaptee _adaptee;
+
+        public Adapter(Adaptee adaptee)
+        {
+            _adaptee = adaptee;
+        }
+        public string GetRequest()
+        {
+            return $"This is '{this._adaptee.GetSpecificRequest()}'";
+        }
+    }
+}
